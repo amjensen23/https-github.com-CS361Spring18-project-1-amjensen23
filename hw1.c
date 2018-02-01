810576c355abb0bd46b84550e61cdd3ff25cab3b
amjensen23 <35844986+amjensen23@users.noreply.github.com>
2018-01-31 20:51:40 -0600

    Update hw1.c

--- hw1.c
+++ hw1.c
@@ -1,18 +1,22 @@
- #include <stdio.h>
-
+#include <stdio.h>
+#include <stdlib.h> // For exit() function
 int main()
 {
-	char netid [7];
-	FILE *fptr;
-	if ((fptr = fopen("netid.md","r")) == NULL){
-       printf("Error! opening file");
-       // Program exits if the file pointer returns NULL.
+    char netid[1000];
+    FILE *fptr;
+
+    if ((fptr = fopen("netid.md", "r")) == NULL)
+    {
+        printf("Error! opening file");
+        // Program exits if file pointer returns NULL.
+        exit(1);         
     }
 
-    fscanf(fptr,"%s", &netid);
-	
-	printf("%s",netid);
-	fclose(fptr); 
-	
-	return 0;
+    // reads text until newline 
+    fscanf(fptr,"%[^\n]", netid);
+
+    printf("%s", netid);
+    fclose(fptr);
+    
+    return 0;
 }
