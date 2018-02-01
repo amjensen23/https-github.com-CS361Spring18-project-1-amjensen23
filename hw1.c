141cb7156ca31c86e27dda7a5b4ba04e1e92ddd3
amjensen23 <35844986+amjensen23@users.noreply.github.com>
2018-01-31 21:30:07 -0600

    Update hw1.c

--- hw1.c
+++ hw1.c
@@ -4,27 +4,23 @@ int main()
 {
   printf("%s", "ajense7");
   
-  all(1, 1, 1);
-  
-  cs361();
-   
-  int us[37];
-  
-  us[0]=0;
-  
-  all(1, us[0], 1);
-  
   return 0;
 }
 
-int all(int your, int are, int belong)
+void all()
 {
-  int to = your+are+belong;
-  
-  return to;
 }
+
+void your;
 
 void cs361()
 {
 }
 
+void are;
+
+void belong;
+
+void to;
+
+int us[37];
