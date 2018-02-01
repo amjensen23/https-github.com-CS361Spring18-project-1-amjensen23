7d129d5a0b0e88a8a7b8d4a6b83a246b20c88006
amjensen23 <35844986+amjensen23@users.noreply.github.com>
2018-01-31 20:41:06 -0600

    Update hw1.c

--- hw1.c
+++ hw1.c
@@ -7,7 +7,6 @@ int main()
 	if ((fptr = fopen("netid.md","r")) == NULL){
        printf("Error! opening file");
        // Program exits if the file pointer returns NULL.
-       exit(1);
     }
 
     fscanf(fptr,"%s", &netid);
