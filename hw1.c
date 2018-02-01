3ae322b04087542065b0a2efc42b4ef2ba3ac155
amjensen23 <35844986+amjensen23@users.noreply.github.com>
2018-01-31 21:18:50 -0600

    Update hw1.c

--- hw1.c
+++ hw1.c
@@ -1,8 +1,24 @@
 #include <stdio.h>
-
+int all()
+{
+}
 int main()
 {
   printf("%s", "ajense7");
+  
+  all();
+  
+  int your;
+  
+  cs361();
+  
+  char are;
+  
+  static int belong;
+  
+  int to;
+  
+  int us[37];
   
   return 0;
 }
