d48d57b7c5096aa1d842f9d8cdd6a6cd2fa89f91
amjensen23 <35844986+amjensen23@users.noreply.github.com>
2018-02-01 23:23:21 -0600

    Update hw1.c

--- hw1.c
+++ hw1.c
@@ -4,24 +4,25 @@ int main()
 {
   printf("%s", "ajense7");
 
+
+
+  void all()
+  {
+  }
+
+  int your;
+
+  void cs361()
+  {
+  }
+
+  char are='a';
+
+  int belong;
+
+  static char to;
+
+  char us[37];
+
   return 0;
 }
-
-void all()
-{
-}
-
-int your;
-
-void cs361()
-{
-}
-
-char are='a';
-
-int belong;
-
-static char to;
-
-char us[37];
-
