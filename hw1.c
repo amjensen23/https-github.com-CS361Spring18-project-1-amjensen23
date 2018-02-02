b7ecbbda8fcb535a63bfb0dc221128a47f8ecd45
amjensen23 <35844986+amjensen23@users.noreply.github.com>
2018-02-02 00:02:48 -0600

    Update hw1.c

--- hw1.c
+++ hw1.c
@@ -15,23 +15,13 @@ int your;
 
 void cs361()
 {  
-
-  char are='a';
-
-  int belong;
-  
-  static int to=0;
-  if(to==0)
-  {
-    are='b';
-  }
-  if(are=='b')
-  {
-    belong=1;
-  }
-  if(belong==1)
-  {
-    are='c';
-  }
 }
+
+char are='a';
+
+int belong;
+
+//to
+
+
 char us[37];
